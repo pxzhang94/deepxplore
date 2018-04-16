b8f6bc308fcbc7a8d5e5f54d0cec72ea1d41b1c0
Jingyi Wang <wangjyee@gmail.com>
2018-04-16 17:49:51 +0800

    restructure a bit.

--- MNIST_mine/gen_mutation.py
+++ MNIST_mine/gen_mutation.py
@@ -14,7 +14,7 @@ import os
 import random
 import numpy as np
 
-def mutation_matrix():
+def mutation_matrix(step_size):
     method = random.randint(1, 3)
     trans_matrix = generate_value(img_rows, img_cols)
     rect_shape = (random.randint(1, 3), random.randint(1, 3))
@@ -29,7 +29,147 @@ def mutation_matrix():
     elif method == 3:
         transformation = c_black(trans_matrix, start_point, rect_shape)
 
-    return transformation * stepsize
+    return transformation * step_size
+
+
+def split_image_train():
+    '''split the training image to normal image (the predication is right)
+        and adversary image (the predication is wrong) and save them to path'''
+
+
+def label_change_mutation(step_size, mutation_number, seed_number):
+    ''' This function returns the label change of random mutations'''
+
+    mutations = []
+    for i in range(mutation_number):
+        mutation = mutation_matrix(step_size)
+        mutations.append(mutation)
+
+    diverged_true = {}
+    diverged_false = {}
+    true = 0
+    false = 0
+    i = 0
+    while true < seed_number or false < seed_number:
+
+
+        # check if the image has been selected, if yes, reuse them
+        # force generate a new batch if force_generate is true
+
+        # if(os.path.exists()):
+        i = i + 1
+        number = random.randint(0, 69999)
+        # ori_img = np.expand_dims(random.choice(x_test), axis=0)
+        ori_img = np.expand_dims(x_test[number], axis=0)
+
+        # first check if input already induces differences
+        ori_label = np.argmax(model.predict(ori_img)[0])
+        if ori_label == y_test[number] and true < args.seeds:
+            true = true + 1
+            ori_img_de = ori_img.copy()
+            orig_img_deprocessed = deprocess_image(ori_img_de)
+
+            # save the result to disk
+            store_path = './mutation_test/' + str(step_size) + "_" + str(mutation_number) + "/true/" + str(i)
+            isExists = os.path.exists(store_path)
+            if not isExists:
+                os.makedirs(store_path)
+            imsave(store_path + "/" + str(ori_label) + '_orig.png', orig_img_deprocessed)
+
+            # if ori_label == y_test[i]:
+            #     store_path = "./test_data/true/"
+            #     isExists = os.path.exists(store_path)
+            #     if not isExists:
+            #         os.makedirs(store_path)
+            #     imsave(store_path + "/" + str(i) + "label" + str(ori_label) + '.png', orig_img_deprocessed)
+            # else:
+            #     store_path = "./test_data/false/"
+            #     isExists = os.path.exists(store_path)
+            #     if not isExists:
+            #         os.makedirs(store_path)
+
+            #     imsave(store_path + "/" + str(i) + "label" + str(y_test[i]) + "_plabel" + str(ori_label) + '.png', orig_img_deprocessed)
+
+            count = 0
+            for j in range(mutation_number):
+                img = ori_img.copy()
+                mu_img = img + mutations[j]
+                mu_label = np.argmax(model.predict(mu_img)[0])
+
+                mu_img_deprocessed = deprocess_image(mu_img)
+                imsave(store_path + "/" + str(j) + "_" + str(mu_label) + '.png', mu_img_deprocessed)
+
+                if mu_label != ori_label:
+                    count += 1
+
+            diverged_true[str(i)] = count
+
+            path = './mutation_test/' + str(step_size) + "_" + str(mutation_number) + "/true/" + str(i) + "/" + str(count)
+            isExists = os.path.exists(path)
+            if not isExists:
+                os.makedirs(path)
+        elif ori_label != y_test[number] and false < seed_number:
+            false = false + 1
+            # same with true
+            ori_img_de = ori_img.copy()
+            orig_img_deprocessed = deprocess_image(ori_img_de)
+
+            # save the result to disk
+            store_path = './mutation_test/' + str(step_size) + "_" + str(mutation_number) + "/false/" + str(i)
+            isExists = os.path.exists(store_path)
+            if not isExists:
+                os.makedirs(store_path)
+            imsave(store_path + "/" + str(ori_label) + '_orig.png', orig_img_deprocessed)
+
+            # if ori_label == y_test[i]:
+            #     store_path = "./test_data/true/"
+            #     isExists = os.path.exists(store_path)
+            #     if not isExists:
+            #         os.makedirs(store_path)
+            #     imsave(store_path + "/" + str(i) + "label" + str(ori_label) + '.png', orig_img_deprocessed)
+            # else:
+            #     store_path = "./test_data/false/"
+            #     isExists = os.path.exists(store_path)
+            #     if not isExists:
+            #         os.makedirs(store_path)
+            #     imsave(store_path + "/" + str(i) + "label" + str(y_test[i]) + "_plabel" + str(ori_label) + '.png', orig_img_deprocessed)
+
+            count = 0
+            for j in range(mutation_number):
+                img = ori_img.copy()
+                mu_img = img + mutations[j]
+                mu_label = np.argmax(model.predict(mu_img)[0])
+
+                mu_img_deprocessed = deprocess_image(mu_img)
+                imsave(store_path + "/" + str(j) + "_" + str(mu_label) + '.png', mu_img_deprocessed)
+
+                if mu_label != ori_label:
+                    count += 1
+
+            diverged_false[str(i)] = count
+
+            path = './mutation_test/' + str(step_size) + "_" + str(mutation_number) + "/false/" + str(i) + "/" + str(
+                count)
+            isExists = os.path.exists(path)
+            if not isExists:
+                os.makedirs(path)
+
+    true_nums = []
+    false_nums = []
+
+    # print("true:", diverged_true)
+    for dt in diverged_true:
+        true_nums.append(diverged_true[dt])
+
+    # print(true_nums)
+
+    # print("false:", diverged_false)
+    for df in diverged_false:
+        false_nums.append(diverged_false[df])
+    # print(false_nums)
+
+    return true_nums,false_nums
+
 
 parser = argparse.ArgumentParser(
     description='Main function for mutation algorithm for input generation in Driving dataset')
@@ -39,14 +179,13 @@ parser.add_argument('seeds', help="number of seeds of input", type=int)
 parser.add_argument('step', help="step size of gradient descent", type=float)
 parser.add_argument('mu_number', help="number of mutation", type=int)
 args = parser.parse_args()
-stepsize = args.step
 
 # input image dimensions
 img_rows, img_cols = 28, 28
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_test = np.concatenate((x_test,x_train),axis=0)
-y_test = np.concatenate((y_test,y_train),axis=0)
+x_test = np.concatenate((x_test, x_train), axis=0)
+y_test = np.concatenate((y_test, y_train), axis=0)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 input_shape = (img_rows, img_cols, 1)
 
@@ -68,111 +207,11 @@ elif args.target_model == 1:
 elif args.target_model == 2:
     model = model3
 
-mutations = []
-for i in range(args.mu_number):
-    mutation = mutation_matrix()
-    mutations.append(mutation)
+[normal_label_change,adv_label_change] = label_change_mutation(args.step,args.mu_number, args.seeds)
 
-diverged_true = {}
-diverged_false = {}
-true = 0
-false = 0
-i = 0
-while true < args.seeds or false < args.seeds:
-    i = i + 1
-    number = random.randint(0,69999)
-    # ori_img = np.expand_dims(random.choice(x_test), axis=0)
-    ori_img = np.expand_dims(x_test[number], axis=0)
+print("Args: ", args)
+print("Normal data label change:")
+print(normal_label_change)
+print("Adv data label change:")
+print(adv_label_change)
 
-    # first check if input already induces differences
-    ori_label = np.argmax(model.predict(ori_img)[0])
-    if ori_label == y_test[number] and true < args.seeds:
-        true = true + 1
-        ori_img_de = ori_img.copy()
-        orig_img_deprocessed = deprocess_image(ori_img_de)
-
-        # save the result to disk
-        store_path = './mutation_test/' + str(stepsize) + "_" + str(args.mu_number) + "/true/" + str(i)
-        isExists = os.path.exists(store_path)
-        if not isExists:
-            os.makedirs(store_path)
-        imsave(store_path + "/" + str(ori_label) + '_orig.png', orig_img_deprocessed)
-
-    # if ori_label == y_test[i]:
-    #     store_path = "./test_data/true/"
-    #     isExists = os.path.exists(store_path)
-    #     if not isExists:
-    #         os.makedirs(store_path)
-    #     imsave(store_path + "/" + str(i) + "label" + str(ori_label) + '.png', orig_img_deprocessed)
-    # else:
-    #     store_path = "./test_data/false/"
-    #     isExists = os.path.exists(store_path)
-    #     if not isExists:
-    #         os.makedirs(store_path)
-    #     imsave(store_path + "/" + str(i) + "label" + str(y_test[i]) + "_plabel" + str(ori_label) + '.png', orig_img_deprocessed)
-
-        count = 0
-        for j in range(args.mu_number):
-            img = ori_img.copy()
-            mu_img = img + mutations[j]
-            mu_label = np.argmax(model.predict(mu_img)[0])
-
-            mu_img_deprocessed = deprocess_image(mu_img)
-            imsave(store_path + "/" + str(j) + "_" + str(mu_label) + '.png', mu_img_deprocessed)
-
-            if mu_label != ori_label:
-                count += 1
-
-        diverged_true[str(i)] = count
-
-        path = './mutation_test/' + str(stepsize) + "_" + str(args.mu_number) + "/true/" + str(i) + "/" + str(count)
-        isExists = os.path.exists(path)
-        if not isExists:
-            os.makedirs(path)
-    elif ori_label != y_test[number] and false < args.seeds:
-        false = false + 1
-        #same with true
-        ori_img_de = ori_img.copy()
-        orig_img_deprocessed = deprocess_image(ori_img_de)
-
-        # save the result to disk
-        store_path = './mutation_test/' + str(stepsize) + "_" + str(args.mu_number) + "/false/" + str(i)
-        isExists = os.path.exists(store_path)
-        if not isExists:
-            os.makedirs(store_path)
-        imsave(store_path + "/" + str(ori_label) + '_orig.png', orig_img_deprocessed)
-
-        # if ori_label == y_test[i]:
-        #     store_path = "./test_data/true/"
-        #     isExists = os.path.exists(store_path)
-        #     if not isExists:
-        #         os.makedirs(store_path)
-        #     imsave(store_path + "/" + str(i) + "label" + str(ori_label) + '.png', orig_img_deprocessed)
-        # else:
-        #     store_path = "./test_data/false/"
-        #     isExists = os.path.exists(store_path)
-        #     if not isExists:
-        #         os.makedirs(store_path)
-        #     imsave(store_path + "/" + str(i) + "label" + str(y_test[i]) + "_plabel" + str(ori_label) + '.png', orig_img_deprocessed)
-
-        count = 0
-        for j in range(args.mu_number):
-            img = ori_img.copy()
-            mu_img = img + mutations[j]
-            mu_label = np.argmax(model.predict(mu_img)[0])
-
-            mu_img_deprocessed = deprocess_image(mu_img)
-            imsave(store_path + "/" + str(j) + "_" + str(mu_label) + '.png', mu_img_deprocessed)
-
-            if mu_label != ori_label:
-                count += 1
-
-        diverged_false[str(i)] = count
-
-        path = './mutation_test/' + str(stepsize) + "_" + str(args.mu_number) + "/false/" + str(i) + "/" + str(count)
-        isExists = os.path.exists(path)
-        if not isExists:
-            os.makedirs(path)
-
-print("true:", diverged_true)
-print("false:", diverged_false)
